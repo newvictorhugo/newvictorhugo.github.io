0390650bf49759761753d393b10159bac133abf9
Victor Hugo <victorhugo10.2003@hotmail.com>
2023-11-06 22:19:25 -0300

    Summary

--- prototiposite/OngAdapvindexswitch/bootstrapSiteADM/js/options.js
+++ prototiposite/OngAdapvindexswitch/bootstrapSiteADM/js/options.js
@@ -49,3 +49,53 @@ nomeInput.addEventListener('input', function() {
 if (nomeInput.value.trim() === '') {
   subtituloNome.textContent = 'Nome';
 }
+
+function botaoFavorito() {
+  var favoritoButton = document.getElementById('favorito-button');
+  var favoritoIcon = document.getElementById('favorito-icon');
+  var iconAtual = favoritoIcon.getAttribute('icon');
+
+  if(iconAtual === 'bi:star'){
+    favoritoIcon.setAttribute('icon', 'bi:star-fill');
+    favoritoIcon.style.color = 'rgb(255, 230, 0)';
+    favoritoButton.style.backgroundColor = 'var(--paleta-cinza)';
+  }else{
+    favoritoIcon.setAttribute('icon', 'bi:star');
+    favoritoIcon.style.color = '';
+    favoritoButton.style.backgroundColor = '';
+  }
+
+}
+document.addEventListener('DOMContentLoaded', toggleFavorito);
+
+document.addEventListener('DOMContentLoaded', function() {
+  var adicionarButton = document.getElementById('adicionar-button');
+
+  adicionarButton.addEventListener('click', function() {
+      var quebraVolunContato = document.querySelector('.quebra-volun-contato');
+      var clonedQuebraVolunContato = quebraVolunContato.cloneNode(true);
+
+      // Limpa os campos clonados (se desejar)
+      var inputFields = clonedQuebraVolunContato.querySelectorAll('input');
+      var selectFields = clonedQuebraVolunContato.querySelectorAll('select');
+
+      inputFields.forEach(function(input) {
+          input.value = ''; // Limpa o valor do campo
+      });
+
+      selectFields.forEach(function(select) {
+          select.value = select.querySelector('option').value; // Define o valor do primeiro item
+      });
+
+      // Insere o clone logo após o original
+      quebraVolunContato.parentNode.insertBefore(clonedQuebraVolunContato, quebraVolunContato.nextSibling);
+
+      // Adiciona evento de clique para o botão "Excluir" no clone
+      var excluirButton = clonedQuebraVolunContato.querySelector('#excluir-button');
+
+      excluirButton.addEventListener('click', function() {
+          clonedQuebraVolunContato.remove(); // Remove o clone quando o botão "Excluir" é clicado
+      });
+      
+  });
+});
