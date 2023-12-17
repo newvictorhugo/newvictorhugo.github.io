02973bb20e1cb3dbac8c25f3e3f12aac872679fe
Victor Hugo <victorhugo10.2003@hotmail.com>
2023-12-17 13:26:10 -0300

    Summary

--- prototiposite/OngAdapvindexswitch/bootstrapSiteADM/js/demo/chart-bars-demo.js
+++ prototiposite/OngAdapvindexswitch/bootstrapSiteADM/js/demo/chart-bars-demo.js
@@ -1,6 +1,6 @@
 var xValues = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"];
 var yValues = [10, 1, 7, 12, 13, 11, 6, 8, 9, 4, 2, 5];
-var barColors = ["black", "orange","blue","green","yellow", "red", "purple", "gold", "green", "pink", "blue", "red"];
+var barColors = ['#2e59d9', '#17a673', '#2c9faf','#2e59d9', '#17a673', '#2c9faf', '#2e59d9', '#17a673', '#2c9faf', '#2e59d9', '#17a673', '#2c9faf'];
 
 new Chart("myChart", {
   type: "bar",
